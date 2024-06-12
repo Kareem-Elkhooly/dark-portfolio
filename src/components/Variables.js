e1810b3840b1ac905f24d501186eab5e709088bb
Kareem El-Khooly <90975727+Kareem-Elkhooly@users.noreply.github.com>
2024-06-13 00:22:05 +0300

    Delete src directory

--- src/components/Variables.js
+++
@@ -1,8 +0,0 @@
-// arabic font family with tailwind
-const arFontFamily = "font-['Tajawal']";
-
-// english font family with tailwind
-const enFontFamily = "font-['manrope']";
-const siteNameEnFont = "font-['able']";
-
-module.exports = {arFontFamily, enFontFamily, siteNameEnFont}
